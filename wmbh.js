7cdc48165be39c1b2290c76243c9db9df20d5412
nip <nip@ubuntu-24.04>
2026-04-27 14:07:12 +0000

    first commit

--- wmbh.js
+++ wmbh.js
@@ -1,1 +1,9 @@
-console.log("Work dude")
+//WIP Button
+const container = document.getElementById("leftcontrols");
+
+const button = document.createElement("button");
+button.className = "btn btn-sm btn-default";
+button.id = "myNewButton";
+button.textContent = "WIP Button";
+
+container.appendChild(button);
