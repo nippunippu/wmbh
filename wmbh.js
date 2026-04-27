14df16b2ab33c2099cc1db2a2acae89b00dfd8c6
nippunippu <77550790+nippunippu@users.noreply.github.com>
2026-04-27 14:26:30 -0300

    Update wmbh.js

--- wmbh.js
+++ wmbh.js
@@ -1,3 +1,5 @@
-(function() {
-    console.log("Nip was here");
-})();
+// Inside your external .js file
+$(document).ready(function() {
+    // Your CyTube customization code here
+    console.log("CyTube loaded and ready!");
+});
