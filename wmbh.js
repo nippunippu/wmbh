5252ab8db62561b03700d4d001d0f4abc56a5506
nippunippu <77550790+nippunippu@users.noreply.github.com>
2026-04-27 11:28:57 -0300

    Update wmbh.js

--- wmbh.js
+++ wmbh.js
@@ -1,11 +1,1 @@
-//WIP Button
-const container = document.getElementById("leftcontrols");
-
-const button = document.createElement("button");
-button.className = "btn btn-sm btn-default";
-button.id = "myNewButton";
-button.textContent = "WIP Button";
-
-container.appendChild(button);
-
-console.log("Nip test!");
+console.log("Nip");
